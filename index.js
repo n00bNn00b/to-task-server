97896106ac93209333090cda72eeb85f10083c6e
n00bNn00b <reldewan9@gmail.com>
2022-07-14 08:37:26 +0600

    try-catch and status of task has been implemented

--- index.js
+++ index.js
@@ -23,22 +23,34 @@ const run = async () => {
   try {
     await client.connect();
     console.log("DB Connected!");
-    const taskCollection = client.db("test").collection("tasks");
+    const taskCollection = client.db("todo").collection("tasks");
     const completedTaskCollection = client
-      .db("test")
+      .db("todo")
       .collection("completedTasks");
 
     //   REST API
     // all tasks
     app.get("/tasks", async (req, res) => {
-      const query = {};
-      const tasks = await taskCollection.find(query).toArray();
-      res.send(tasks);
-      // console.log(tasks);
+      try {
+        const query = {};
+        const tasks = await taskCollection.find(query).toArray();
+        res.send(tasks);
+        // console.log(tasks);
+      } catch (err) {
+        res.send(err);
+      }
     });
 
     // completed task
-
+    app.get("/completed", async (req, res) => {
+      try {
+        const query = {};
+        const tasks = await completedTaskCollection.find(query).toArray();
+        res.send(tasks);
+      } catch (err) {
+        res.send(err);
+      }
+    });
     // task by id
     app.get("/task/:id", async (req, res) => {
       try {
@@ -52,65 +64,87 @@ const run = async () => {
     });
     // single task filter by email
     app.get("/tasks/:email/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const task = await taskCollection.findOne(query);
-      res.send(task);
+      try {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const task = await taskCollection.findOne(query);
+        res.send(task);
+      } catch (err) {
+        res.send(err);
+      }
     });
 
     // tasks by individual user
     app.get("/tasks/:email", async (req, res) => {
-      const email = req.params.email;
-      const filter = { email: email };
-      const cursor = taskCollection.find(filter);
-      const tasks = await cursor.toArray();
-      res.send(tasks);
+      try {
+        const email = req.params.email;
+        const filter = { email: email };
+        const cursor = taskCollection.find(filter);
+        const tasks = await cursor.toArray();
+        res.send(tasks);
+      } catch (err) {
+        res.send(err);
+      }
     });
 
     // post request by user
     app.post("/tasks", async (req, res) => {
-      const taskName = req.body;
-      const query = {
-        email: taskName.email,
-        task: taskName.taskName,
-      };
-      const exists = await taskCollection.findOne(query);
-      if (exists) {
-        return res.send({ success: false, task: exists });
+      try {
+        const taskName = req.body;
+        const query = {
+          email: taskName.email,
+          task: taskName.taskName,
+          status: taskName.status,
+        };
+        const exists = await taskCollection.findOne(query);
+        if (exists) {
+          return res.send({ success: false, task: exists });
+        }
+        const result = await taskCollection.insertOne(taskName);
+        return res.send({ success: true, result });
+      } catch (err) {
+        res.send(err);
       }
-      const result = await taskCollection.insertOne(taskName);
-      return res.send({ success: true, result });
     });
     // completed tasks post
     app.post("/completed", async (req, res) => {
-      const task = req.body;
-      const query = {
-        email: task.email,
-        taskName: task.taskName,
-      };
-      const exists = await completedTaskCollection.findOne(query);
-      if (exists) {
-        return res.send({
-          success: false,
-          task: exists,
+      try {
+        const task = req.body;
+        const query = {
+          email: task.email,
+          taskName: task.taskName,
+          status: task.status,
+        };
+        const exists = await completedTaskCollection.findOne(query);
+        if (exists) {
+          return res.send({
+            success: false,
+            task: exists,
+          });
+        }
+        const result = await completedTaskCollection.insertOne(task);
+        res.send({
+          success: true,
+          result,
         });
+      } catch (err) {
+        res.send(err);
       }
-      const result = await completedTaskCollection.insertOne(task);
-      res.send({
-        success: true,
-        result,
-      });
     });
 
     // delete item
     app.delete("/tasks/:email/:id", async (req, res) => {
-      const id = req.params.id;
-      const email = req.params.email;
-      console.log(id, email);
-      const query = { _id: ObjectId(id), email: email };
-      const removedTask = await taskCollection.deleteOne(query);
-      res.send(removedTask);
-      console.log(removedTask);
+      try {
+        const id = req.params.id;
+        const email = req.params.email;
+        console.log(id, email);
+        const query = { _id: ObjectId(id), email: email };
+        const removedTask = await taskCollection.deleteOne(query);
+        res.send(removedTask);
+        console.log(removedTask);
+      } catch (err) {
+        res.send(err);
+      }
     });
   } finally {
     //
